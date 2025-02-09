ed4863e6cffbbbc6722bf9a5d0a37311f449c426
Hana Dusíková <hanicka@hanicka.net>
2025-02-09 04:04:38 +0100

    use file_sets in cmake, use targets to set pedantic/c++26 instead of global settings or weird macros

--- cmake/pedantic-and-modern.cmake
+++ cmake/pedantic-and-modern.cmake
@@ -1,24 +1,18 @@
-set(PEDANTIC_FLAGS)
-set(PEDANTIC_DEFINITIONS)
+# link to this library if you want your code to be compiled in a strict/pedantic mode
+add_library(opt_pedantic INTERFACE)
+add_library(opt::pedantic ALIAS opt_pedantic)
 
 if (MSVC)
-  list(APPEND PEDANTIC_FLAGS "/W4" "/WX")
+  target_compile_options(opt_pedantic INTERFACE "/W4" "/WX")
 else()
-  list(APPEND PEDANTIC_FLAGS  "-Wall" "-Wextra" "-pedantic" "-Wshadow" "-Wconversion" "-Werror")
-	
-	if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
-    list(APPEND PEDANTIC_DEFINITIONS "_LIBCPP_ENABLE_NODISCARD")
+  target_compile_options(opt_pedantic INTERFACE "-Wall" "-Wextra" "-pedantic" "-Wshadow" "-Wconversion" "-Werror")
+  
+  if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+    target_compile_definitions(opt_pedantic INTERFACE "_LIBCPP_ENABLE_NODISCARD")
 	endif()
 endif()
 
-# this macro sets c++26 and pedantic flags for specified target
-# it doesn't need to be set for everything if visibility is "PUBLIC" it will be inherited
-# by any library which links to these targets, but not in other direction
-macro(modern_and_pedantic_settings TARGET VISIBILITY)
-  if (NOT VISIBILITY)
-    set(VISIBILITY PUBLIC)
-  endif()
-  target_compile_features(${TARGET} ${VISIBILITY} cxx_std_26)
-  target_compile_options(${TARGET} ${VISIBILITY} "$<$<COMPILE_LANGUAGE:CXX>:${PEDANTIC_FLAGS}>")
-  target_compile_definitions(${TARGET} ${VISIBILITY} "$<$<COMPILE_LANGUAGE:CXX>:${PEDANTIC_DEFINITIONS}>")
-endmacro()
+# you want to use cool modern stuff
+add_library(opt_c++26 INTERFACE)
+add_library(opt::c++26 ALIAS opt_c++26)
+target_compile_features(opt_c++26 INTERFACE cxx_std_26)
