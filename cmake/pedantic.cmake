e57b7c3ace9f9b93cf3c34314510fbe77790d661
Hana Dusíková <hanicka@hanicka.net>
2025-02-09 03:19:27 +0100

    better propagate pedantic flags, switch from global to target specific (with inheriting it downstream)

--- cmake/pedantic.cmake
+++ cmake/pedantic.cmake
@@ -1,19 +1,24 @@
-macro(add_c_and_cxx_compile_options)
-	add_compile_options("$<$<COMPILE_LANGUAGE:C>:${ARGV}>")
-	add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:${ARGV}>")
-endmacro()
-
-macro(add_c_and_cxx_compile_definitions)
-	add_compile_definitions("$<$<COMPILE_LANGUAGE:C>:${ARGV}>")
-	add_compile_definitions("$<$<COMPILE_LANGUAGE:CXX>:${ARGV}>")
-endmacro()
+set(PEDANTIC_FLAGS)
+set(PEDANTIC_DEFINITIONS)
 
 if (MSVC)
-	add_c_and_cxx_compile_options("/W4" "/WX")
+  list(APPEND PEDANTIC_FLAGS "/W4" "/WX")
 else()
-	add_c_and_cxx_compile_options("-Wall" "-Wextra" "-pedantic" "-Wshadow" "-Wconversion" "-Werror")
-
+  list(APPEND PEDANTIC_FLAGS  "-Wall" "-Wextra" "-pedantic" "-Wshadow" "-Wconversion" "-Werror")
+	
 	if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
-		add_c_and_cxx_compile_definitions("_LIBCPP_ENABLE_NODISCARD")
+    list(APPEND PEDANTIC_DEFINITIONS "_LIBCPP_ENABLE_NODISCARD")
 	endif()
 endif()
+
+# this macro sets c++26 and pedantic flags for specified target
+# it doesn't need to be set for everything if visibility is "PUBLIC" it will be inherited
+# by any library which links to these targets, but not in other direction
+macro(modern_and_pedantic_settings TARGET VISIBILITY)
+  if (NOT VISIBILITY)
+    set(VISIBILITY PUBLIC)
+  endif()
+  target_compile_features(${TARGET} ${VISIBILITY} cxx_std_26)
+  target_compile_options(${TARGET} ${VISIBILITY} "$<$<COMPILE_LANGUAGE:CXX>:${PEDANTIC_FLAGS}>")
+  target_compile_definitions(${TARGET} ${VISIBILITY} "$<$<COMPILE_LANGUAGE:CXX>:${PEDANTIC_DEFINITIONS}>")
+endmacro()
