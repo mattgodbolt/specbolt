52a683cd1232b4657a97aaac81700b036d11faa2
Matt Godbolt <matt@godbolt.org>
2025-04-01 09:32:08 -0500

    Introduce loading from Internet Archive (#25)
    * Move some things to typescript
    * Pretty heinous crimes againt UI design

--- web/main.js
+++ web/main.js
@@ -1,10 +1,47 @@
+import _ from "lodash";
+
 import {Spectrum, SpectrumShift, SymbolShift} from "./spectrum";
 
-const $canvas = document.querySelector('canvas');
-const $effectiveMhz = document.querySelector('#effective-mhz');
-const $currentFps = document.querySelector('#current-fps');
+const $canvas = document.querySelector("canvas");
+const $effectiveMhz = document.querySelector("#effective-mhz");
+const $currentFps = document.querySelector("#current-fps");
+const $loadGameButton = document.querySelector(".load-game-button");
+if ($loadGameButton) {
+    const $loadGameDialog = document.querySelector("dialog.game-finder");
+    $loadGameButton.addEventListener("click", () => { $loadGameDialog.showModal(); });
+    const $searchResults = $loadGameDialog.querySelector(".search-results");
+    const $template = $loadGameDialog.querySelector("template").content;
+    function updateSearchResults(results) {
+        $searchResults.innerHTML = "";
+        if (results.length === 0) {
+            $searchResults.innerHTML = "<h3>No results</h3>";
+        }
+        for (const result of results) {
+            const newItem = $template.cloneNode(true);
+            newItem.querySelector(".description").innerHTML = result.title;
+            newItem.querySelector(".description").addEventListener("click", async (evt) => {
+                evt.preventDefault();
+                $loadGameDialog.requestClose();
+                const z80FileUrl = new URL(await downloadZ80For(result.identifier));
+                await spectrum.loadSnapshot(z80FileUrl);
+                // TODO start spectrum?
+                updateUrl({load : z80FileUrl});
+            });
+            $searchResults.appendChild(newItem);
+        }
+    }
+    const $searchButton = $loadGameDialog.querySelector(".search-input");
+    async function doSearch() {
+        $searchResults.innerHTML = "<h5>Loading...</h5>";
+        const results = await searchForSpectrum($searchButton.value);
+        updateSearchResults(results);
+    }
+    const debouncedSearch = _.debounce(doSearch, 500);
+    $searchButton.addEventListener("change", debouncedSearch);
+    $searchButton.addEventListener("keyup", debouncedSearch);
+}
 
-const spectrum = new Spectrum($canvas, document.querySelector('#warning'));
+const spectrum = new Spectrum($canvas, document.querySelector("#warning"));
 
 class Typist {
     constructor(text) {
@@ -14,19 +51,19 @@ class Typist {
         let counter = 10;
         while (index < text.length) {
             let code = text.charCodeAt(index++);
-            if (code >= 'A'.charCodeAt(0) && code <= 'Z'.charCodeAt(0)) {
+            if (code >= "A".charCodeAt(0) && code <= "Z".charCodeAt(0)) {
                 this.onCycle.set(counter, {code : SpectrumShift, pressed : true});
                 this.onCycle.set(counter + 7, {code : SpectrumShift, pressed : false});
                 counter += 2;
                 code = code + 32;
             }
-            if (code === '^'.charCodeAt(0)) {
+            if (code === "^".charCodeAt(0)) {
                 this.onCycle.set(counter, {code : SymbolShift, pressed : true});
                 this.onCycle.set(counter + 5, {code : SymbolShift, pressed : false});
                 counter += 2;
                 continue;
             }
-            if (code === '$'.charCodeAt(0))
+            if (code === "$".charCodeAt(0))
                 code = 13;
             this.onCycle.set(counter, {code : code, pressed : true});
             this.onCycle.set(counter + 3, {code : code, pressed : false});
@@ -40,6 +77,16 @@ class Typist {
         if (todo)
             spectrum.setKeyState(todo.code, todo.pressed);
     }
+}
+
+function updateUrl(updateMap) {
+    const parsedQuery = new URLSearchParams(window.location.search);
+    for (const item of Object.keys(updateMap)) {
+        parsedQuery.set(item, updateMap[item]);
+    }
+    const url = new URL(window.location);
+    url.search = parsedQuery.toString();
+    window.history.pushState({}, undefined, url.toString());
 }
 
 async function initialise() {
@@ -59,13 +106,13 @@ async function initialise() {
 
     let startOnKeyPress = false;
 
-    document.onkeydown = (e) => {
+    $canvas.onkeydown = (e) => {
         if (startOnKeyPress) {
             spectrum.start();
         }
         spectrum.onKeyDown(e);
     };
-    document.onkeyup = (e) => { spectrum.onKeyUp(e); }
+    $canvas.onkeyup = (e) => { spectrum.onKeyUp(e); };
 
     if (parsedQuery.get("load")) {
         const game_url = new URL(parsedQuery.get("load"), window.location);
@@ -87,19 +134,51 @@ async function initialise() {
     if (parsedQuery.get("type")) {
         const typist = new Typist(parsedQuery.get("type"));
 
-        $canvas.addEventListener('emulated-frame', () => { typist.type(); });
+        $canvas.addEventListener("emulated-frame", () => { typist.type(); });
     }
 }
 
 initialise().then(() => {});
 
 // JavaScript inside of an iframe embedded within reveal.js
-window.addEventListener('message', (event) => {
-    if (event.data === 'specbolt:start') {
+window.addEventListener("message", (event) => {
+    if (event.data === "specbolt:start") {
         spectrum.start();
-    } else if (event.data === 'slide:start') {
+    } else if (event.data === "slide:start") {
         // Does nothing...
-    } else if (event.data === 'slide:stop' || event.data === 'specbolt:stop') {
+    } else if (event.data === "slide:stop" || event.data === "specbolt:stop") {
         spectrum.stop();
     }
 });
+
+const ArchiveOrgCors = "https://cors.archive.org";
+async function findSpectrumZ80Files(itemId) {
+    const metadataUrl = `${ArchiveOrgCors}/metadata/${itemId}`;
+    const response = await fetch(metadataUrl);
+    const data = await response.json();
+
+    const z80Files =
+        data.files.filter((file) => file.name.toLowerCase().endsWith(".z80") || file.format?.toLowerCase() === "z80");
+
+    return z80Files;
+}
+
+function getCorsFileDownloadUrl(itemId, fileName) { return `${ArchiveOrgCors}/cors/${itemId}/${fileName}`; }
+async function downloadZ80For(itemId) {
+    const files = await findSpectrumZ80Files(itemId);
+    if (files.length === 1) {
+        return getCorsFileDownloadUrl(itemId, files[0].name);
+    }
+}
+
+async function searchForSpectrum(query) {
+    const searchQuery = encodeURI(`title:(${query}) AND collection:(softwarelibrary_zx_spectrum)`);
+    const searchUrl = `${ArchiveOrgCors}/advancedsearch.php?q=${searchQuery}&fl[moo]=identifier,title&output=json`;
+    const response = await fetch(searchUrl);
+    const data = await response.json();
+    return data.response.docs;
+}
+
+// console.log(await do_the_thing("zx_Scuba_Dive_1983_Durell_Software_a"));
+// const mongoose = await searchForSpectrumEmulator("jet set");
+// console.log(mongoose);
